e7bc5c21177602c8d7880867852f0524e0ab39e5
lemaiwo <wouterlemaire120@hotmail.com>
2022-07-31 22:07:26 +0200

    first commit

---
+++ generators/app/index.js
@@ -0,0 +1,195 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const path = require("path");
+const glob = require("glob");
+const semver = require("semver");
+const packageJson = require('package-json');
+
+module.exports = class extends Generator {
+    static displayName = "Create a new OpenUI5/SAPUI5 library";
+
+    prompting() {
+
+        const minFwkVersion = {
+            OpenUI5: "1.90.1", //"1.60.0",
+            SAPUI5: "1.90.0" //"1.77.0"
+        };
+
+        const fwkDependencies = {
+            OpenUI5: "@openui5/ts-types-esm",
+            SAPUI5: "@sapui5/ts-types-esm"
+        };
+
+        // Have Yeoman greet the user.
+        if (!this.options.embedded) {
+            this.log(
+                yosay(`Welcome to the ${chalk.red("generator-ui5-ts-library")} generator!`)
+            );
+        }
+
+        const prompts = [
+            {
+                type: "input",
+                name: "libraryname",
+                message: "How do you want to name this library?",
+                validate: s => {
+                    if (/^\d*[a-zA-Z][a-zA-Z0-9]*$/g.test(s)) {
+                        return true;
+                    }
+
+                    return "Please use alpha numeric characters only for the library name.";
+                },
+                default: "myUI5Library"
+            },
+            {
+                type: "input",
+                name: "namespace",
+                message: "Which namespace do you want to use?",
+                validate: s => {
+                    if (/^[a-zA-Z0-9_.]*$/g.test(s)) {
+                        return true;
+                    }
+
+                    return "Please use alpha numeric characters and dots only for the namespace.";
+                },
+                default: "com.myorg"
+            },
+            {
+                type: "list",
+                name: "framework",
+                message: "Which framework do you want to use?",
+                choices: ["OpenUI5", "SAPUI5"],
+                default: "OpenUI5"
+            },
+            {
+                when: response => {
+                    this._minFwkVersion = minFwkVersion[response.framework];
+                    return true;
+                },
+                type: "input", // HINT: we could also use the version info from OpenUI5/SAPUI5 to provide a selection!
+                name: "frameworkVersion",
+                message: "Which framework version do you want to use?",
+                default: async (answers) => {
+                    const npmPackage = fwkDependencies[answers.framework];
+                    try {
+                        return (await packageJson(npmPackage)).version;
+                    } catch (ex) {
+                        chalk.red('Failed to lookup latest version for ${npmPackage}! Fallback to min version...')
+                        return minFwkVersion[answers.framework];
+                    }
+                },
+                validate: v => {
+                    return (
+                        (v && semver.valid(v) && semver.gte(v, this._minFwkVersion)) ||
+                        chalk.red(
+                            `Framework requires the min version ${this._minFwkVersion}!`
+                        )
+                    );
+                }
+            },
+            {
+                type: "input",
+                name: "author",
+                message: "Who is the author of the library?",
+                default: this.user.git.name()
+            },
+            {
+                type: "confirm",
+                name: "newdir",
+                message: "Would you like to create a new directory for the library?",
+                default: true
+            }
+        ];
+
+        return this.prompt(prompts).then(props => {
+            // To access props later use this.props.someAnswer;
+            // this.props = props;
+            if (props.newdir) {
+                this.destinationRoot(`${props.namespace}.${props.libraryname}`);
+            }
+
+            this.config.set(props);
+            this.config.set("framework", props.framework);
+
+            // determine the ts-types and version
+            this.config.set("tstypes", fwkDependencies[props.framework]);
+            this.config.set("tstypesVersion", props.frameworkVersion);
+
+            this.config.set("namespaceURI", props.namespace.split(".").join("/"));
+            this.config.set(
+                "librarynamespace",
+                `${props.namespace}.${props.libraryname}`
+            );
+            this.config.set(
+                "librarynamespaceURI",
+                this.config
+                    .get("librarynamespace")
+                    .split(".")
+                    .join("/")
+            );
+            this.config.set(
+                "librarybasepath",
+                this.config
+                    .get("librarynamespace")
+                    .split(".")
+                    .map(_ => "..")
+                    .join("/") + "/"
+            );
+            this.config.set("frameworklowercase", props.framework.toLowerCase());
+        });
+    }
+
+    writing() {
+        const oConfig = this.config.getAll();
+
+        this.sourceRoot(path.join(__dirname, "templates"));
+        glob
+            .sync("**", {
+                cwd: this.sourceRoot(),
+                nodir: true
+            })
+            .forEach(file => {
+                const sOrigin = this.templatePath(file);
+                let sTarget = this.destinationPath(
+                    file
+                        .replace(/^_/, "")
+                        .replace("baselibrary", oConfig.librarynamespaceURI)
+                        .replace(/\/_/, "/")
+                );
+
+                this.fs.copyTpl(sOrigin, sTarget, oConfig);
+            });
+    }
+
+    install() {
+        this.config.set("setupCompleted", true);
+        this.installDependencies({
+            bower: false,
+            npm: true
+        });
+    }
+
+    end() {
+        this.spawnCommandSync("git", ["init", "--quiet"], {
+            cwd: this.destinationPath()
+        });
+        this.spawnCommandSync("git", ["add", "."], {
+            cwd: this.destinationPath()
+        });
+        this.spawnCommandSync(
+            "git",
+            [
+                "commit",
+                "--quiet",
+                "--allow-empty",
+                "-m",
+                "Initialize repository with UI5 Library Generator"
+            ],
+            {
+                cwd: this.destinationPath()
+            }
+        );
+    }
+};
---
+++ generators/app/templates/readme.md
@@ -0,0 +1,35 @@
+# UI5 Library `<%= libraryname %>`
+
+Insert the purpose of this project and some interesting info here...
+
+## Requirements
+
+* git client, Node.js
+
+## Installation / Setup
+
+```sh
+npm i
+```
+
+## Usage
+
+Start the control sample page in watch mode:
+
+```sh
+npm run watch
+```
+
+This opens the example control sample page in a browser window and launches the project in watch mode, which triggers several things at once whenever any code is changed:
+* A re-generation of the TypeScript interfaces for the controls (so TypeScript knows all the generated control methods)
+* A transpilation of the TypeScript code to JavaScript
+* A reload of the page displayed in the browser
+
+This generator is based on the following sample: [https://github.com/SAP-samples/ui5-typescript-control-library](https://github.com/SAP-samples/ui5-typescript-control-library).
+
+## License
+
+This project is licensed under the Apache Software License, version 2.0 except as noted otherwise in the [LICENSE](LICENSE) file.
+
+---------------------------
+###### This template is provided to you by Wouter Lemaire
